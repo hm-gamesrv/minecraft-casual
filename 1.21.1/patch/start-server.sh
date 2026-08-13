f99558ba941d9d1d85d1b160c73d0b4a20659e6b
HiroMuraki <419734782@qq.com>
2026-08-13 19:08:55 +0800

    修正 start-server.sh：使用 exec

--- 1.21.1/patch/start-server.sh
+++ 1.21.1/patch/start-server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -euo pipefail
+
+cd /app
 
 if [ ! -d ./world ] || [ -z "$(ls -A ./world 2>/dev/null)" ]; then
     mkdir -p ./world
     cp -r ./datapacks ./world/
 fi
 
-java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.248/unix_args.txt "$@"
+exec java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.248/unix_args.txt "$@"
