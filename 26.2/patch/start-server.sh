e9fb83963d2af677bd90257bccae8767c1596cf1
HiroMuraki <419734782@qq.com>
2026-08-29 15:27:47 +0800

    修正启动脚本

--- 26.2/patch/start-server.sh
+++ 26.2/patch/start-server.sh
@@ -22,9 +22,13 @@ set_property online-mode "$GAME_ONLINE_MODE"
 set_property management-server-secret "$GAME_MANAGEMENT_SERVER_SECRET"
 set_property resource-pack "$GAME_RESOURCE_PACK"
 
-JAVA_ARGS="-Xms${JAVA_MEMORY} -Xmx${JAVA_MEMORY} -XX:+UseZGC"
-if [ -n "$GAME_AUTHLIB_INJECTOR_URL" ]; then
-    JAVA_ARGS="${JAVA_ARGS} -javaagent:authlib-injector-1.2.8.jar=${GAME_AUTHLIB_INJECTOR_URL}"
-fi
+{
+    printf '%s%s\n' '-Xms' "${JAVA_MEMORY}"
+    printf '%s%s\n' '-Xmx' "${JAVA_MEMORY}"
+    printf '%s\n' '-XX:+UseZGC'
+    if [ -n "$GAME_AUTHLIB_INJECTOR_URL" ]; then
+        printf '%s"%s"\n' '-javaagent:authlib-injector-1.2.8.jar=' "$GAME_AUTHLIB_INJECTOR_URL"
+    fi
+} > /app/user_jvm_args.txt
 
-exec java "${JAVA_ARGS}" -jar fabric-server.jar --nogui
+exec java @/app/user_jvm_args.txt -jar fabric-server.jar --nogui
